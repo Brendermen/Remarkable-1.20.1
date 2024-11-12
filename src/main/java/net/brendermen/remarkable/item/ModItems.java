f3a9a474d02783f12f9315bf47881a20e002f054
Brendermen <brendermenMinecraft@gmail.com>
2024-11-12 08:43:31 -0600

    Brendermen

--- src/main/java/net/brendermen/remarkable/item/ModItems.java
+++ src/main/java/net/brendermen/remarkable/item/ModItems.java
@@ -20,4 +20,22 @@ public class ModItems {
 
     public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SILVER_NUGGET = ITEMS.register("silver_nugget",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DIAMOND_SHARD = ITEMS.register("diamond_shard",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> EMERALD_SHARD = ITEMS.register("emerald_shard",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> REDSTONE_CHUNK = ITEMS.register("redstone_chunk",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_IRON_NUGGET = ITEMS.register("raw_iron_nugget",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_GOLD_NUGGET = ITEMS.register("raw_gold_nugget",
+            () -> new Item(new Item.Properties()));
 }
